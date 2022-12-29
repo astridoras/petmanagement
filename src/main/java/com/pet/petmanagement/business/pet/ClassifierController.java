cc62a973d330c5e9af0f17f46f872c9b7a90af13
astridoras <astrid.oras@gmail.com>
2022-12-29 09:11:48 +0200

    Mappers fixed

--- src/main/java/com/pet/petmanagement/business/pet/ClassifierController.java
+++ src/main/java/com/pet/petmanagement/business/pet/ClassifierController.java
@@ -1,18 +1,23 @@
 package com.pet.petmanagement.business.pet;
 
 import com.pet.petmanagement.domain.color.Color;
+import com.pet.petmanagement.domain.color.ColorJson;
+import com.pet.petmanagement.domain.color.ColorMapper;
 import com.pet.petmanagement.domain.color.ColorService;
 import com.pet.petmanagement.domain.country.Country;
+import com.pet.petmanagement.domain.country.CountryJson;
+import com.pet.petmanagement.domain.country.CountryMapper;
 import com.pet.petmanagement.domain.country.CountryService;
 import com.pet.petmanagement.domain.type.Type;
+import com.pet.petmanagement.domain.type.TypeJson;
+import com.pet.petmanagement.domain.type.TypeMapper;
 import com.pet.petmanagement.domain.type.TypeService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
 
-//TODO fix
-@CrossOrigin(maxAge = 3600)
+
 
 @RestController
 @RequestMapping("/")
@@ -24,21 +29,30 @@ public class ClassifierController {
     @Resource
     private TypeService typeService;
 
+    @Resource
+    private ColorMapper colorMapper;
+
+    @Resource
+    private CountryMapper countryMapper;
+
+    @Resource
+    private TypeMapper typeMapper;
+
     @GetMapping("countries")
-    public List<Country> getAllCountries() {
-        //Todo mapper
-        return countryService.findAllCountries();
+    public List<CountryJson> getAllCountries() {
+        List<Country> countries = countryService.findAllCountries();
+        return countryMapper.countriesToCountryJson(countries);
     }
 
     @GetMapping("types")
-    public List<Type> getAllTypes() {
-        //Todo mapper
-        return typeService.findAllTypes();
+    public List<TypeJson> getAllTypes() {
+        List<Type> types = typeService.findAllTypes();
+       return typeMapper.typesToTypeJson(types);
     }
 
     @GetMapping("colors")
-    public List<Color> getAllColors() {
-        //Todo mapper
-        return colorService.findAllColors();
+    public List<ColorJson> getAllColors() {
+        List<Color> colors = colorService.findAllColors();
+        return colorMapper.colorsToColorJson(colors);
     }
 }
