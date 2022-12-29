7605023f021621dbbcba093f1c438f8304ea3672
astridoras <astrid.oras@gmail.com>
2022-12-29 09:10:48 +0200

    Mappers fixed

--- src/main/java/com/pet/petmanagement/domain/country/CountryMapper.java
+++ src/main/java/com/pet/petmanagement/domain/country/CountryMapper.java
@@ -2,7 +2,11 @@ package com.pet.petmanagement.domain.country;
 
 import org.mapstruct.*;
 
+import java.util.List;
+
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface CountryMapper {
      CountryJson countryToCountryJson(Country country);
+     List<CountryJson> countriesToCountryJson(List<Country>countries);
+
 }
--- src/main/java/com/pet/petmanagement/domain/type/TypeMapper.java
+++ src/main/java/com/pet/petmanagement/domain/type/TypeMapper.java
@@ -1,8 +1,13 @@
 package com.pet.petmanagement.domain.type;
 
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface TypeMapper {
     TypeJson typeToTypeJson(Type type);
+
+    List<TypeJson> typesToTypeJson(List<Type> types);
 }
