67110cf1ba840cad41e197d37e2266b421394f45
astridoras <astrid.oras@gmail.com>
2022-12-29 09:09:20 +0200

    WebConfig file for Global CORS Configuration

--- src/main/java/com/pet/petmanagement/domain/color/ColorMapper.java
+++ src/main/java/com/pet/petmanagement/domain/color/ColorMapper.java
@@ -1,8 +1,13 @@
 package com.pet.petmanagement.domain.color;
 
+import com.pet.petmanagement.business.pet.json.PetResponseJson;
+import com.pet.petmanagement.domain.pet.Pet;
 import org.mapstruct.*;
+
+import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface ColorMapper {
     ColorJson colorToColorJson(Color color);
+    List<ColorJson> colorsToColorJson(List<Color>colors);
 }
